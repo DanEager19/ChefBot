6174dd141a9d03c25822709cceabb4004bf6ccce
Daniel Eager <daneager19@gmail.com>
2021-11-01 17:34:47 -0500

    Version 2.0

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # ChefBot
 Cooking and Gardening Club Discord bot reminder.<br />
-auth.json excluded as it contains sensitive info.<br />
-Node files also excluded to prevent bloat.<br />
-Currently Hosted on a DigitalOcean Droplet<br />
+Currently Hosted on a Raspberry Pi<br />
 Commands:
  - !start - Begin the countdown. 
+ 
+ Deprecated/Legacy - will be added again ata  later date.
  - !loc - Update the location. Gets the current location if no 2nd argument provided.
  - !time - Update the time. Gets the current (set) time if no 2nd argument provided.
  - !day - Update the day. Gets the current (set) day if no 2nd argument provided.
--- index.ts
+++ index.ts
@@ -2,9 +2,10 @@ require('dotenv').config()
 const Discord = require('discord.js');
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
 
+const prefix = '!'
 const cook = 'Cooking'
 const garden = 'Gardening'
-const announcements = 889222322819567716
+const announcements = '889222322819567716'
 
 let meetings = {
     section: new String,
@@ -17,10 +18,8 @@ client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}.`)
 })
 
-client.on("messageCreate", msg => {
-    const weekly = setInterval( () => {
-        const timeNow = new Date().getTime
-        console.log(`The time is${timeNow}; the message is being sent...`)
+client.on("messageCreate", async(msg) => {
+    let weekly = setInterval( () => {
         if(meetings.week % 2 === 0) meetings.section = cook
         else meetings.section = garden
         client.channels.cache
@@ -28,29 +27,14 @@ client.on("messageCreate", msg => {
         .send(`Meeting today! ${meetings.section} is meeting at ${meetings.time} in ${meetings.location}!`)
         meetings.week++
     }, 604800000)
-
-    if (msg.content.substr(0,1) === '!') {
-        switch (msg.content.substr(2)) {
-            case 'start':
-                async () => weekly
-                break
-            case 'time':
-                meetings.time = msg.substr(6)
-                console.log(`Update time to ${meetings.time}...`)
-                break
-            case 'loc':
-                meetings.location = msg.substr(5)
-                console.log(`Update location to ${meetings.location}...`)
-                break
-            case 'stop':
-                clearInterval(weekly)
-                break
-            case 'anc':
-                client.channels.cache
-                .get(announcements)
-                .send(msg.substr(5))
-                break
-        }
+    
+    if (!msg.content.startsWith(prefix)) return;
+    
+    const args = msg.content.trim().split(/ +/g)
+    const cmd = args[0].slice(prefix.length).toLowerCase()
+    if(cmd === 'start') {
+        console.log('Starting...')
+        async () => weekly
     }
 })
 
