284823b310b4c6866fa000f433cc4e3a0553b078
Daniel Eager <daneager19@gmail.com>
2021-11-01 15:43:31 -0500

    Finished skeleton

--- index.ts
+++ index.ts
@@ -2,8 +2,9 @@ require('dotenv').config()
 const Discord = require('discord.js');
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
 
-const cook = 'Cooking';
-const garden = 'Gardening';
+const cook = 'Cooking'
+const garden = 'Gardening'
+const announcements = '889222322819567716'
 
 let meetings = {
     section: new String,
@@ -22,25 +23,38 @@ client.on("messageCreate", msg => {
         console.log(`The time is${timeNow}; the message is being sent...`)
         if(meetings.week % 2 === 0) meetings.section = cook
         else meetings.section = garden
-        msg.send(`Meeting today! ${meetings.section} is meeting at ${meetings.time} in ${meetings.location}!`)
+        client.channels.cache
+        .get(announcements)
+        .send(`Meeting today! ${meetings.section} is meeting at ${meetings.time} in ${meetings.location}!`)
         meetings.week++
     }, 604800000)
 
     if (msg.content.substr(0,1) === '!') {
         switch (msg.content.substr(2)) {
             case 'start':
-                async () => weekly 
+                async () => weekly
+                break
             case 'time':
                 meetings.time = msg.substr(6)
+                console.log(`Update time to ${meetings.time}...`)
+                break
             case 'loc':
                 meetings.location = msg.substr(5)
+                console.log(`Update location to ${meetings.location}...`)
+                break
             case 'stop':
                 clearInterval(weekly)
+                break
+            case 'anc':
+                client.channels.cache
+                .get(announcements)
+                .send(msg.substr(5))
+                break
         }
     }
 })
 
-client.on("error", error =>{
+client.on('error', error => {
     console.error(`This error occured: ${error}`)
 })
 
