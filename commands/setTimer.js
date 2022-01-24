7fe2f6c73610144105798cc1865a349b38900d52
Daniel <daneager19@gmail.com>
2022-01-24 16:44:44 -0600

    Pls

--- commands/setTimer.js
+++ commands/setTimer.js
@@ -1,23 +1,13 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { Client, Collection, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
-const { token } = require('../config.json');
+const wait = require('util').promisify(setTimeout);;
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('timer')
 		.setDescription('Sets a timer'),
 	async execute(interaction) {
-		client.login(token);
-		function delay(ms) { 
-			return new Promise(resolve => setTimeout(resolve, ms));
-		}
-		async function timeout() {
-			await delay(5000);
-			const channel = await client.channels.fetch('889222322819567716');
-			await channel.send("My Message");
-		}	
 		await interaction.reply('Pong!');
-		timeout();
+		await wait(2000);
+		await interaction.followUp('Pong Again!');
 	},
 };
