ecd3367f8303526b4314e525510821b378acdc32
Dan Eager <81655709+DanEager19@users.noreply.github.com>
2022-07-17 16:24:33 -0500

    Added reserve

---
+++ src/commands/Reserve.ts
@@ -0,0 +1,26 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+const axios = require('axios')
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('reserve')
+        .setDescription('Reserve an item based on ID.')
+        .addIntegerOption((option) => option.setName('id').setDescription('Enter item ID.'))
+        .addStringOption((option) => option.setName('email').setDescription('Enter your email.')),
+    async execute(interaction:any) {
+        axios.post('http://localhost:3000/reserve', {
+                itemId: interaction.options.getInteger('id'),
+                email: interaction.options.getString('email')
+            })
+            .then(async(res: any) => {
+                const content = JSON.stringify(res.data);
+                await interaction.reply({
+                    content
+                });
+            })
+            .catch((e: Error) => {
+                console.log(`[X] - ${e}`);
+            });
+    }
+        
+}
