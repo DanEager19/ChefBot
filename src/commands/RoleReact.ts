98390e18d94e8db105b7adae43a88bb1039c5baa
Dan Eager <81655709+DanEager19@users.noreply.github.com>
2022-07-23 20:25:12 -0500

    Collector bug

--- src/commands/RoleReact.ts
+++ src/commands/RoleReact.ts
@@ -7,63 +7,49 @@ export const RoleReact = {
         .setDescription('Allows for member role reactions'),
 	async execute(interaction: CommandInteraction) {
         if (interaction.user.bot) return;
-        const member = interaction.guild?.members.cache
-            .find((member: { id: string; }) => member.id === interaction.user.id);
+        const content = 'Done!';
+        await interaction.reply({content, ephemeral: true})
 
-        const officerRole = member?.roles.cache.get('785951435829149827');
-        const adminRole = member?.roles.cache.get('785964719914614814');
+        const text = '**React with an emoji for the corresponding role!**\n\n🍽: `Cooks`\n\n🌱: `Gardeners`\n\n💻: `Social`\n\n☢: `Offtopic`'
+        const message = await interaction.channel?.send(text);
 
-        if(typeof(adminRole) === 'undefined' && typeof(officerRole) === 'undefined') {
-            const content = 'Sorry! You don\'t have access to this command!';
-            console.log(`[x] - ${interaction.user.tag} tried to use the rolereact command.`);
-            await interaction.reply({
-                content,
-                ephemeral: true
-            });
-        } else {
-            const content = 'Done!';
-            await interaction.reply({content, ephemeral: true})
+        message?.react('🍽')
+            .then(() => { message.react('🌱') })
+            .then(() => { message.react('💻') })
+            .then(() => { message.react('☢') });
 
-            const text = '**React with an emoji for the corresponding role!**\n\n🍽: `Cooks`\n\n🌱: `Gardeners`\n\n💻: `Social`\n\n☢: `Offtopic`'
-            const message = await interaction.channel?.send(text);
-
-            message?.react('🍽')
-                .then(() => { message.react('🌱') })
-                .then(() => { message.react('💻') })
-                .then(() => { message.react('☢') });
-
-            const filter = (reaction: any, user: any) => {
-                const reactEmoji = reaction.emoji.name;
-                return ('🍽' === reactEmoji|| '🌱'=== reactEmoji || '💻'=== reactEmoji || '☢' === reactEmoji) && user.id === interaction.user.id;
-            }
-
-            const collector = message?.createReactionCollector({ filter });
-
-            collector?.on('collect', async(reaction:any, user:any) => {
-                const { guild } = reaction.message 
-                const member = guild.members.cache.find((member: { id: string; }) => member.id === user.id);
-                if (reaction.emoji.name === '🍽') {
-                    const role = reaction.message.guild.roles.cache.find((r: { id: string; }) => r.id === '785959812475256832');
-                    member.roles.add(role);
-                    console.log(`[+] - Gave ${user.tag} the ${role} role`);
-                } else if (reaction.emoji.name === '🌱') {
-                    const role = reaction.message.guild.roles.cache.find((r: { id: string; }) => r.id === '785959816676900884');
-                    member.roles.add(role);
-                    console.log(`[+] - Gave ${user.tag} the ${role} role`);
-                } else if (reaction.emoji.name === '💻') {
-                    const role = reaction.message.guild.roles.cache.find((r: { id: string; }) => r.id === '999077403299172463');
-                    member.roles.add(role);
-                    console.log(`[+] - Gave ${user.tag} the ${role} role`);
-                } else if (reaction.emoji.name === '☢') {
-                    const role = reaction.message.guild.roles.cache.find((r: { id: string; }) => r.id === '796871266329165824');
-                    member.roles.add(role);
-                    console.log(`[+] - Gave ${user.tag} the ${role} role`);
-                }
-            });
-            
-            collector?.on('end', () => {
-                console.log(`Collection of role reactions has ended.`);
-            });
+        const filter = (reaction: any, user: any) => {
+            const reactEmoji = reaction.emoji.name;
+            return ('🍽' === reactEmoji|| '🌱'=== reactEmoji || '💻'=== reactEmoji || '☢' === reactEmoji) && user.id === interaction.user.id;
         }
+
+        const collector = message?.createReactionCollector({ filter });
+
+        collector?.on('collect', async(reaction:any, user:any) => {
+            const { guild } = reaction.message 
+            const member = guild.members.cache.find((member: { id: string; }) => member.id === user.id);
+            if (reaction.emoji.name === '🍽') {
+                const role = reaction.message.guild.roles.cache.find((r: { id: string; }) => r.id === '785959812475256832');
+                member.roles.add(role);
+                console.log(`[+] - Gave ${user.tag} the ${role} role`);
+            } else if (reaction.emoji.name === '🌱') {
+                const role = reaction.message.guild.roles.cache.find((r: { id: string; }) => r.id === '785959816676900884');
+                member.roles.add(role);
+                console.log(`[+] - Gave ${user.tag} the ${role} role`);
+            } else if (reaction.emoji.name === '💻') {
+                const role = reaction.message.guild.roles.cache.find((r: { id: string; }) => r.id === '999077403299172463');
+                member.roles.add(role);
+                console.log(`[+] - Gave ${user.tag} the ${role} role`);
+            } else if (reaction.emoji.name === '☢') {
+                const role = reaction.message.guild.roles.cache.find((r: { id: string; }) => r.id === '796871266329165824');
+                member.roles.add(role);
+                console.log(`[+] - Gave ${user.tag} the ${role} role`);
+            }
+        });
+        
+        collector?.on('end', () => {
+            console.log(`Collection of role reactions has ended.`);
+        });
+        
     },
 };
